339a3f623ee6e776d865d42d9ab0317b819dac5c
Balaji <rao.jbalaji@gmail.com>
2020-10-10 01:47:07 +0800

    facebook reaxt app material UI build process

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './theme';
 
+// Component.
+import Header from './components/Header';
 const browserHistory = createBrowserHistory();
 
 import {
@@ -13,17 +17,20 @@ import {
 export default function App() {
     return (
         <Fragment>
-            <Router history={browserHistory} basename="/facebook-react-app">
-                <h1>Simple Banking App goes here</h1>
-                <Switch>
-                    <Route path="/dashboard">
-                        <Dashboard />
-                    </Route>
-                    <Route path="/">
-                        <SignIn />
-                    </Route>
-                </Switch>
-            </Router>
+            <ThemeProvider theme={theme}>
+                <Header />
+                <Router history={browserHistory} basename="/facebook-react-app">
+                    <Switch>
+                        <Route exact path="/">
+                            <SignIn />
+                        </Route>
+                        <Route exact path="/dashboard" basename="/facebook-react-app">
+                            <Dashboard />
+                        </Route>
+                    </Switch>
+                </Router>
+            </ThemeProvider>
         </Fragment>
     );
 };
+
