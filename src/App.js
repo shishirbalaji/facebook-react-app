40071dc1ba97099798ff48c66054e1a401cf0f3a
Balaji <rao.jbalaji@gmail.com>
2020-10-10 01:37:22 +0800

    adding router functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+const browserHistory = createBrowserHistory();
+
+import {
+    Dashboard,
+    SignIn
+} from './modules';
+
 
 export default function App() {
     return (
         <Fragment>
-            Simple Banking App (facebook react app) goes here
+            <Router history={browserHistory} basename="/facebook-react-app">
+                <h1>Simple Banking App goes here</h1>
+                <Switch>
+                    <Route path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                    <Route path="/">
+                        <SignIn />
+                    </Route>
+                </Switch>
+            </Router>
         </Fragment>
     );
 };
